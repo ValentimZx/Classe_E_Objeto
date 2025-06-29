aa0a7c48176f8a11161d4045271f84a6721e4893
ValentimZx <nvalentim824@gmail.com>
2025-06-29 16:15:37 -0500

    Commit da secao 4

---
+++ Classe-E-Objetos/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("## Classe e objetos ##");
+
+int x = 100;
+
+Pessoa p1 = new Pessoa();
+p1.nome = "Nicolas";
+p1.idade = 21;
+p1.sexo = "Masculino";
+
+Console.WriteLine($"Meu nome e {p1.nome} e eu tenho {p1.idade} e sou do sexo {p1.sexo}");
+
+Console.ReadKey();
+
+class Pessoa
+{
+    public string? nome;
+    public int idade;
+    public string? sexo;
+}
